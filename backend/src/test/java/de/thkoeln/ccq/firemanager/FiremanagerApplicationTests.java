249047db9704cf35d405824c9718ecc0bf9a1abc
Jascha Wendt <151759172+Gonnie08@users.noreply.github.com>
2026-07-10 22:12:34 +0200

    fix: allow quality gate failure temporarily and fix checkstyle indentation

--- backend/src/test/java/de/thkoeln/ccq/firemanager/FiremanagerApplicationTests.java
+++ backend/src/test/java/de/thkoeln/ccq/firemanager/FiremanagerApplicationTests.java
@@ -2,10 +2,11 @@ package de.thkoeln.ccq.firemanager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 class FiremanagerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
