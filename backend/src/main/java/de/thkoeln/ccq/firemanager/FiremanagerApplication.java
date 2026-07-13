b3a9a62502f429aeab2662745c69539f128e194f
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2026-07-13 12:40:42 +0200

    feat: implement backend CI workflow and add Checkstyle configuration

--- backend/src/main/java/de/thkoeln/ccq/firemanager/FiremanagerApplication.java
+++ backend/src/main/java/de/thkoeln/ccq/firemanager/FiremanagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FiremanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FiremanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FiremanagerApplication.class, args);
+    }
 
 }
