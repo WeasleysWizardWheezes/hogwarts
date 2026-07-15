e7478a2e87f81afe9ce1cacbaee37b8ec10bbd3f
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2026-07-15 12:58:53 +0200

    feat: api first setup

--- frontend/src/shared/api/client.ts
+++ frontend/src/shared/api/client.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch"
-import type { paths } from "./schema"
+import type { paths } from "@weasleyswizardwheezes/hogwarts-api"
 
 export const api = createClient<paths>({
   baseUrl: import.meta.env.VITE_API_URL ?? "",
