62f888ef52bfa6c6c0abf6f3257b4f160afff3c8
annikaCescotti <100763702+annikaCescotti@users.noreply.github.com>
2026-08-10 20:53:14 +0200

    refactor: genrate api locally

--- frontend/src/shared/api/client.ts
+++ frontend/src/shared/api/client.ts
@@ -1,5 +1,5 @@
 import createClient from "openapi-fetch"
-import type { paths } from '@weasleyswizardwheezes/hogwarts-api-client'
+import type { paths } from './schema'
 
 export const api = createClient<paths>({
   baseUrl: import.meta.env.VITE_API_URL ?? "",
